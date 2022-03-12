e97383c09fad7b85e9ff47c3444512606c7a742d
quentin-marc <quentinmarc81@gmail.com>
2022-03-12 18:32:04 +0100

    form contact v1

---
+++ frontend/src/creneauxRecuperation/creneauxRecuperation.jsx
@@ -0,0 +1,57 @@
+import './creneauxRecuperation.css';
+
+const CreneauxRecuperation = ({ }) => {
+
+    var listeCreneaux = [
+        {
+            nomJour: "lundi",
+            dateJour: "14 / 03",
+            creaneauxDuJour: ["8h00", "9h00", "10h00", "11h00", "12h00"]
+        },{
+            nomJour: "mardi",
+            dateJour: "15 / 03",
+            creaneauxDuJour: ["8h00", "9h00", "10h00", "11h00", "12h00"]
+        },{
+            nomJour: "mercredi",
+            dateJour: "16 / 03",
+            creaneauxDuJour: ["8h00", "9h00", "10h00", "11h00", "12h00"]
+        },{
+            nomJour: "jeudi",
+            dateJour: "17 / 03",
+            creaneauxDuJour: ["8h00", "9h00", "10h00", "11h00", "12h00"]
+        },{
+            nomJour: "vendredi",
+            dateJour: "18 / 03",
+            creaneauxDuJour: ["8h00", "9h00", "10h00", "11h00", "12h00"]
+        },{
+            nomJour: "samedi",
+            dateJour: "19 / 03",
+            creaneauxDuJour: ["8h00", "9h00", "10h00", "11h00", "12h00"]
+        }
+    ]
+
+    return(
+        <div id='boxCreneauxRecuperation'>
+            <h1>Quand souhaitez-vous récupérer vos courses ?</h1>
+            <div>Adresse de récupération: TODO mettre adresse</div>
+            <div id='creneauxRecuperation'>
+                {listeCreneaux.map((jour, index) => {
+                    return <div className='jour' key={index}>
+                                <div className='nomJour'>{jour.nomJour}</div>
+                                <div className='dateJour'>{jour.dateJour}</div>
+                                {jour.creaneauxDuJour.map((creneau, indexCreneau) => {
+                                    return <div className='creneau' key={indexCreneau}>{creneau}</div>
+                                })}
+                            </div>
+                })}
+            </div>
+            <h1>Comment pouvons-nous vous contacter ?</h1>
+            <div>Ces informations ne seront utilisées que pour la gestion de cette commande. Elles ne seront pas utilisées à des fins publicitaires.</div>
+            <input type='email'></input>
+            <input type='phone'></input>
+            <button>Valider ma commande</button>
+        </div>
+    );
+};
+
+export default CreneauxRecuperation;
