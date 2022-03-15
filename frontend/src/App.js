f0a6be7ceab7377f7eb7d10150c21b5f68a09c34
quentin-marc <quentinmarc81@gmail.com>
2022-03-15 13:05:58 +0100

    fin front v0 pc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,30 @@ import FormulaireProduits from './formulaireProduits/formulaireProduits.jsx';
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        Header
-      </header>
       <div id="mainContener">
         <FormulaireProduits></FormulaireProduits>
         <FormulaireCreneaux></FormulaireCreneaux>
       </div>
-      <footer className="footer">
-        Footer
+      <footer>
+        <div className='subtitle'>Notre équipe</div>
+        <div className='createurs'>
+          <div className='createur'>
+            <img className='photoCreateur' src='./images/corentin.png'></img>
+            <div className='nomCreateur'>Corentin<br></br>Branchereau</div>
+          </div>  
+          <div className='createur'>
+            <img className='photoCreateur' src='./images/qandria.png'></img>
+            <div className='nomCreateur'>Quentin<br></br>De Andria</div>
+          </div> 
+          <div className='createur'>
+            <img className='photoCreateur' src='./images/qmarc.png'></img>
+            <div className='nomCreateur'>Quentin<br></br>Marc</div>
+          </div> 
+          <div className='createur'>
+            <img className='photoCreateur' src='./images/louis.png'></img>
+            <div className='nomCreateur'>Louis<br></br>Rob</div>
+          </div> 
+        </div>
       </footer>
     </div>
   );
--- frontend/src/formulaireProduits/formulaireProduits.jsx
+++ frontend/src/formulaireProduits/formulaireProduits.jsx
@@ -14,6 +14,53 @@ const FormulaireProduits = ({ }) => {
         document.getElementById('formulaireProduits').style.display = "none";
         document.getElementById('formulaireCreneaux').style.display = "block";
     }
+
+    //listing en dur des produits disponibles
+    // var listeProduits = [
+    //     {
+    //         nom: 'Pommes',
+    //         image: 'pommes.svg',
+    //         prix: 2.3,
+    //         unite: "kg",
+    //         type: "fruit"
+    //     },{
+    //         nom: 'Beurre doux (250g)',
+    //         image: 'beurre.svg',
+    //         prix: 2.3,
+    //         unite: "unite",
+    //         type: "cremerie"
+    //     },{
+    //         nom: 'Blettes',
+    //         image: 'blettes.svg',
+    //         prix: 2.3,
+    //         unite: "kg",
+    //         type: "legumes"
+    //     },{
+    //         nom: 'Carottes',
+    //         image: 'carottes.svg',
+    //         prix: 2.3,
+    //         unite: "kg",
+    //         type: "legumes"
+    //     },{
+    //         nom: 'Celeri',
+    //         image: 'celeri.svg',
+    //         prix: 2.3,
+    //         unite: "kg",
+    //         type: "legumes"
+    //     },{
+    //         nom: 'Chou',
+    //         image: 'chou.svg',
+    //         prix: 2.3,
+    //         unite: "unite",
+    //         type: "legumes"
+    //     },{
+    //         nom: 'Choux de Bruxelle',
+    //         image: 'choux_bruxelle.svg',
+    //         prix: 2.3,
+    //         unite: "kg",
+    //         type: "legumes"
+    //     }
+    // ];
     
     useEffect(() => {
         const getListeProduits = async () => {
@@ -35,6 +82,8 @@ const FormulaireProduits = ({ }) => {
 
     return(
         <div id='formulaireProduits'>
+            <h1>Commandez vos produits locaux depuis chez vous</h1>
+            <div className='subtitle'>Bienvenue sur NOM SITE, nous sommes une petite équipe d’ingénieurs voulant aider à (re)développer les circuits courts. Ce drive vient juste de naitre, le projet va s'améliorer progressivement.</div>
             <div id="nosProduits">
                 {!listeProduits ? null : listeProduits.map((produit, i) => {       
                     return (<Produit donnees={produit} key={i} />) 
